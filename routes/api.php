534c447464b678973034206509b5e7c02a170698
laurentBsss <danone97200@gmail.com>
2017-11-22 23:31:20 +0100

    add new web route and  api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,20 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// API ROUTES ==================================  
+// since we will be using this just for CRUD, we won't need create and edit
+    // Angular will handle both of those forms
+    // this ensures that a user can't access api/create or api/edit when there's nothing there
+
+Route::resource('comments', 'CommentController', 
+        array('only' => array('index', 'store', 'destroy')));
+
+// CATCH ALL ROUTE =============================  
+// all routes that are not home or api will be redirected to the frontend 
+// this allows angular to route them 
+
+Route::any('/', function()
+{
+    return View::make('index'); 
+});
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/ici', function() { return 'Je suis la page ici !'; });
+
+// HOME PAGE ===================================  
+// we dont need to use Laravel Blade 
+// we will return a PHP file that will hold all of our Angular content
+// see the "Where to Place Angular Files" below to see ideas on how to structure your app return  
+Route::get('/', function() {   
+    View::make('index'); // will return app/views/index.php 
+});
